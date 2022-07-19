f026bd58d2227ddc14a9134412c671143bdb96a7
Dhruv Rana <dhruvrana0000@gmail.com>
2022-07-19 16:20:26 +0530

    Getting the dynamic value from the URL using next js useRouter hook

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,7 +1,9 @@
 //nested Routes // our-domain.com/news/something-important
-
+import { useRouter } from "next/router";
 const SomethingImportant = () => {
-  return <h1>The important Page</h1>;
+  const router = useRouter();
+  const newsId = router.query.newsId; //using the dynamic page name identifier to get the value from the URL
+  return <h1>The {newsId} Page</h1>;
 };
 
 export default SomethingImportant;
