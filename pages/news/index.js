f9d6fcae43307099731de7020ca3590023a6997b
Dhruv Rana <dhruvrana0000@gmail.com>
2022-07-19 16:28:09 +0530

    Using links in Next.js using Link Component

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,21 @@
 //our-domain.com/news
-
+import Link from "next/link";
 const NewsPage = () => {
-  return <h1>NEWS Page</h1>;
+  return (
+    <>
+      <h1>NEWS Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/next-js-is-a-great-framework">
+            next-js-is-a-great-framework
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/something-else">Something Else</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
